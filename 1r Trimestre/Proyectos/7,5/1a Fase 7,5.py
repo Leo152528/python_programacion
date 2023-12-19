9bab365e6328fd140363e027e262c1262ea149eb
Leo152528 <leonardo.sun@campus.xaloc.org>
2023-12-19 10:08:19 +0100

    Create 1a Fase 7,5.py

---
+++ 1r Trimestre/Proyectos/7,5/1a Fase 7,5.py
@@ -0,0 +1,32 @@
+#Programa el código que gestione una partida. El programa ofrece una carta, el usuario puede o no
+#aceptar. La puntuación se acumula según el número de carta que aparece al azar. Si consigues que:
+#La puntuación de 7.5, el programa te felicita. "Enhorabuena, has ganado la partida"
+#Si te pasas de 7.5, el programa informa: "Has perdido la partida!"
+#Si te plantas con una puntuación entre 6 y 7 "Has sido un poco conservador"
+#Si te plantas con una puntuación inferior a 6. "Quizás tendrías que arriesgar un poco ¿no?
+#El programa te debe dar la opción de repetir o no una nueva partida.
+import random
+continuar="s"
+otra="s"
+while continuar=="s":
+    total=0
+    while otra=="s" or total<7.5:
+        carta=random.randint(1,12)
+        while carta==8 or carta==9:
+            carta=random.randint(1,12)
+        print("Tu carta es: ",carta)
+        if carta>=10:
+            total=total+0,5
+        else:
+            total=total+carta
+        print("Tu suma es de: ",total)
+        otra=input("Quieres otra carta s/n: ")
+    if total==7.5:
+        print("Enhorabuena, has ganado la partida")
+    elif total>7.5:
+        print("¡Has perdido la partida!")
+    elif total>=6 and total<=7:
+        print("Has sido un poco conservador")
+    else:
+        print("Quizás tendrías que arriesgar un poco ¿no?")
+    continuar=input("Introduce si quieres otra parida s/n:")
